7b149726dd3e5cd4a0cf9627f31ffc12dab5ce9d
Javier Garcia <garcjavi@oregonstate.edu>
2022-10-27 15:18:45 -0700

    Added tables/entities handlebars.

--- server.js
+++ server.js
@@ -9,24 +9,18 @@ var PORT = process.env.PORT || 8320;
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-
-
-console.log("line 22");
-
 app.get('/', function (req, res, next) {
 	console.log("HOME");
 	var homeData = db[0];
-
 	res.status(200).render('homePage', { // homePage is homePage.js auto-generated js
 		overview: homeData.overview
 	})
 });
 
-
-
 app.use(express.static('public')); // ????????
 
 app.get('/clients', function(req, res, next) {
+	console.log("CLIENTS");
 	var clientsData = db[1];
 
 	if (clientsData) {
@@ -38,6 +32,57 @@ app.get('/clients', function(req, res, next) {
   }
 });
 
+app.get('/sales', function(req, res, next) {
+	console.log("SALES");
+	var salesData = db[2];
+
+	if (salesData) {
+		res.status(200).render('salesPage', {
+			sales: salesData.sales
+    })
+  } else {
+    next();
+  }
+});
+
+app.get('/planets', function(req, res, next) {
+	console.log("PLANETS");
+	var planetsData = db[3];
+
+	if (planetsData) {
+		res.status(200).render('planetsPage', {
+			planets: planetsData.planets
+    })
+  } else {
+    next();
+  }
+});
+
+app.get('/information', function(req, res, next) {
+	console.log("INFORMATION");
+	var informationData = db[4];
+
+	if (informationData) {
+		res.status(200).render('informationPage', {
+			information: informationData.information
+    })
+  } else {
+    next();
+  }
+});
+
+app.get('/funfacts', function(req, res, next) {
+	console.log("FUN FACTS");
+	var funFactsData = db[5];
+
+	if (funFactsData) {
+		res.status(200).render('funFactsPage', {
+			funFacts: funFactsData.funFacts
+    })
+  } else {
+    next();
+  }
+});
 
 app.get('*', function (req, res) {
 	console.log("ERROR");
@@ -47,6 +92,5 @@ app.get('*', function (req, res) {
 });
 
 app.listen(PORT, function () {
-	//console.log(db[0])
 	console.log("== Server is listening on PORT", PORT);
 });
